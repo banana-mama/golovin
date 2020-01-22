67a4c85bff4b4b6c5887d92c75e0319b73a96b70
quize <quize.web@gmail.com>
2020-01-22 21:10:55 +0300

    added: mongo;

--- app/repository/index.php
+++ app/repository/index.php
@@ -1,8 +1,15 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 //phpinfo();
+//exit();
 
 ### REDIS TEST
+
+echo 'REDIS TEST:1';
 
 try {
   $redis = new Redis();
@@ -20,6 +27,30 @@ $testValue = $redis->get('testKey');
 
 echo '<pre>';
 print_r($testValue);
+echo '</pre>';
+
+### MONGODB TEST
+
+echo ('MONGODB TEST:' . PHP_EOL);
+
+$mongoDBmanager = new \MongoDB\Driver\Manager('mongodb://root:secret@172.19.0.15:27017');
+
+$testCommand = new MongoDB\Driver\Command(['ping' => 1]);
+try {
+  $testCursor = $mongoDBmanager->executeCommand('golovin', $testCommand);
+} catch(MongoDB\Driver\Exception $e) {
+  exit('Ошибка подключения к mongodb: ' . $e);
+}
+
+$test = $testCursor->toArray()[0];
+echo '<pre>';
+print_r($test);
+echo '</pre>';
+
+$command = new MongoDB\Driver\Command(['listDatabases' => 1]);
+$cursor = $mongoDBmanager->executeCommand('admin', $command);
+echo '<pre>';
+print_r($cursor);
 echo '</pre>';
 
 ###
