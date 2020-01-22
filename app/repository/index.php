8053e80a3a5c4a9eeb7768ab68994e9f96410106
quize <quize.web@gmail.com>
2020-01-22 20:01:55 +0300

    added: redis;

--- app/repository/index.php
+++ app/repository/index.php
@@ -1,5 +1,27 @@
 <?php
 
-phpinfo();
+//phpinfo();
+
+### REDIS TEST
+
+try {
+  $redis = new Redis();
+  $redis->connect('172.19.0.10', 6379);
+} catch (RedisException $e) {
+  exit('Ошибка подключения к redis: ' . $e);
+}
+
+echo '<pre>';
+print_r($redis->time());
+echo '</pre>';
+
+$redis->set('testKey','testValue');
+$testValue = $redis->get('testKey');
+
+echo '<pre>';
+print_r($testValue);
+echo '</pre>';
+
+###
 
 exit();
